31fe15f2e1680b2235553edcfc87f4b2e8521f05
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2021-10-17 16:04:09 +0300

    add adaptive and burger-menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector('.scheme-svg');
 const totalPriceTag = document.querySelector('.price-total');
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.menu');
 
 let cost = 800;
 let totalPrice = 0;
@@ -12,4 +14,8 @@ schemeSvg.addEventListener('click', (event) => {
         totalPrice = totalSits * cost;
         totalPriceTag.textContent = totalPrice;
     }
+})
+
+menuButton.addEventListener('click', () => {
+    menu.classList.toggle('is-open');
 })
